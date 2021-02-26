d076b86fa06b5ff8143ae4726b8eb953a53e72dd
ishakhimsurya <ishakhimsurya29@gmail.com>
2021-02-26 21:41:42 +0530

    added Temporary User model

---
+++ app/src/main/java/com/digitalpathology/digi_report/Object/User.java
@@ -0,0 +1,46 @@
+package com.digitalpathology.digi_report.Object;
+
+class User {
+
+    String name;
+    String email;
+    String phone;
+
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
